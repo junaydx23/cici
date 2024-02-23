6804f30c5ef428f9b0d5aef0b29610e1ea6dc812
junaydx23 <154425793+junaydx23@users.noreply.github.com>
2024-02-23 15:19:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,107 @@ const { RsnChat } = require("rsnchat");
 const rsnchat = new RsnChat("rsnai_Ps4ryTcQkXFnjX6nmFV2BZ7y");
 
 const app = express();
-const PORT = process.env.PORT || 22660;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/gpt4', async (req, res) => {
+app.get('/chatgpt', async (req, res) => {
   try {
     const prompt = req.query.prompt;
 
     const response = await rsnchat.gpt(prompt);
 
+    const gptResponse = response.message;
+
+    res.json({ response: gptResponse });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/bing', async (req, res) => {
+  try {
+    const prompt = req.query.prompt;
+
+    const response = await rsnchat.bing(prompt);
 
     const gptResponse = response.message;
 
+    res.json({ response: gptResponse });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/mixtral', async (req, res) => {
+  try {
+    const prompt = req.query.prompt;
+
+    const response = await rsnchat.mixtral(prompt);
+
+    const gptResponse = response.message;
+
+    res.json({ response: gptResponse });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/codellama', async (req, res) => {
+  try {
+    const prompt = req.query.prompt;
+
+    const response = await rsnchat.codellama(prompt);
+
+    const gptResponse = response.message;
+
+    res.json({ response: gptResponse });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/llama', async (req, res) => {
+  try {
+    const prompt = req.query.prompt;
+
+    const response = await rsnchat.llama(prompt);
+
+    const gptResponse = response.message;
+
+    res.json({ response: gptResponse });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/gemini', async (req, res) => {
+  try {
+    const prompt = req.query.prompt;
+
+    const response = await rsnchat.gemini(prompt);
+
+    const gptResponse = response.message;
+
+    res.json({ response: gptResponse });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/openchat', async (req, res) => {
+  try {
+    const prompt = req.query.prompt;
+
+    const response = await rsnchat.openchat(prompt);
+
+    const gptResponse = response.message;
 
     res.json({ response: gptResponse });
   } catch (error) {
