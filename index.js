c354c2487b24f720a506085afb493b69a935903e
junaydx23 <154425793+junaydx23@users.noreply.github.com>
2024-02-24 14:09:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { RsnChat } = require("rsnchat");
 const rsnchat = new RsnChat("rsnai_Ps4ryTcQkXFnjX6nmFV2BZ7y");
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
